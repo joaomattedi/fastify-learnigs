61a666d38679420cad19c25b7adbecce804fff36
Joao <joaommattedi@gmail.com>
2023-07-23 14:57:52 -0300

    eslint install

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/hello',() => {
+app.get('/hello', () => {
   return 'Hello Node!'
 })
 
-app.listen({
-  port:3333
-}).then(() => {
-  console.log('Server running!');
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server running!')
+  })
