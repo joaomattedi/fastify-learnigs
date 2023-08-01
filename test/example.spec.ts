c027ee24f1717f8695f23f38abba180c3ce8774c
Joao <joaommattedi@gmail.com>
2023-08-01 20:20:10 -0300

    example test

---
+++ test/example.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest'
+
+test('o usuário consegue criar uma nova transação', () => {
+  // exemplo
+
+  const responseCode = 201
+
+  expect(responseCode).toEqual(201)
+})
