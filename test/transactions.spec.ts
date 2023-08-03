6cb34172bce63b47be83860394219c72a4d2fa2d
Joao <joaommattedi@gmail.com>
2023-08-03 20:32:24 -0300

    refact test file

---
+++ test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { test, beforeAll, afterAll, describe } from 'vitest'
+import { app } from '../src/app'
+import request from 'supertest'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('user can create new transactions', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
