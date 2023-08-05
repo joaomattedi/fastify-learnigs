f17a3bcb914e39ec01fcc525f7fecee2999d8adf
Joao <joaommattedi@gmail.com>
2023-08-05 08:36:24 -0300

    install tsup to build the project

---
+++ test/transactions.spec.js
@@ -0,0 +1,99 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const vitest_1 = require("vitest");
+const app_1 = require("../src/app");
+const supertest_1 = __importDefault(require("supertest"));
+const child_process_1 = require("child_process");
+(0, vitest_1.describe)('Transactions routes', () => {
+    (0, vitest_1.beforeAll)(async () => {
+        await app_1.app.ready();
+    });
+    (0, vitest_1.afterAll)(async () => {
+        await app_1.app.close();
+    });
+    (0, vitest_1.beforeEach)(() => {
+        (0, child_process_1.execSync)('npm run knex migrate:rollback --all');
+        (0, child_process_1.execSync)('npm run knex migrate:latest');
+    });
+    (0, vitest_1.test)('user can create new transactions', async () => {
+        await (0, supertest_1.default)(app_1.app.server)
+            .post('/transactions')
+            .send({
+            title: 'New Transaction',
+            amount: 5000,
+            type: 'credit',
+        })
+            .expect(201);
+    });
+    (0, vitest_1.it)('should be able to list all transactions', async () => {
+        const createTransactionsResponse = await (0, supertest_1.default)(app_1.app.server)
+            .post('/transactions')
+            .send({
+            title: 'New Transaction',
+            amount: 5000,
+            type: 'credit',
+        });
+        const cookies = createTransactionsResponse.get('Set-Cookie');
+        const listTransactionsResponse = await (0, supertest_1.default)(app_1.app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200);
+        (0, vitest_1.expect)(listTransactionsResponse.body.transactions).toEqual([
+            vitest_1.expect.objectContaining({
+                title: 'New Transaction',
+                amount: 5000,
+            }),
+        ]);
+    });
+    (0, vitest_1.it)('should be able to get a specific transaction', async () => {
+        const createTransactionsResponse = await (0, supertest_1.default)(app_1.app.server)
+            .post('/transactions')
+            .send({
+            title: 'New Transaction',
+            amount: 5000,
+            type: 'credit',
+        });
+        const cookies = createTransactionsResponse.get('Set-Cookie');
+        const listTransactionsResponse = await (0, supertest_1.default)(app_1.app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200);
+        const transactionId = listTransactionsResponse.body.transactions[0].id;
+        const getTransactionResponse = await (0, supertest_1.default)(app_1.app.server)
+            .get(`/transactions/${transactionId}`)
+            .set('Cookie', cookies)
+            .expect(200);
+        (0, vitest_1.expect)(getTransactionResponse.body.transaction).toEqual(vitest_1.expect.objectContaining({
+            title: 'New Transaction',
+            amount: 5000,
+        }));
+    });
+    (0, vitest_1.it)('should be able to get the summary', async () => {
+        const createTransactionsResponse = await (0, supertest_1.default)(app_1.app.server)
+            .post('/transactions')
+            .send({
+            title: 'Credit Transaction',
+            amount: 5000,
+            type: 'credit',
+        });
+        const cookies = createTransactionsResponse.get('Set-Cookie');
+        await (0, supertest_1.default)(app_1.app.server)
+            .post('/transactions')
+            .set('Cookie', cookies)
+            .send({
+            title: 'Debit Transaction',
+            amount: 2000,
+            type: 'debit',
+        });
+        const summaryResponse = await (0, supertest_1.default)(app_1.app.server)
+            .get('/transactions/summary')
+            .set('Cookie', cookies)
+            .expect(200);
+        (0, vitest_1.expect)(summaryResponse.body.summary).toEqual({
+            amount: 3000,
+        });
+    });
+});
